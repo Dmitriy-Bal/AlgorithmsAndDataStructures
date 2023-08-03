0177ce106e6d0ca187523fb1002fbab4797121a4
Dmitriy <118436365+Dmitriy-Bal@users.noreply.github.com>
2023-08-03 19:57:16 +0300

    Dz_3
    Добавил решение домашнего задания №3, к семинару №4

---
+++ HomeWork/RedBlackTree/Main.java
@@ -0,0 +1,45 @@
+package org.example.RedBlackTree;
+
+import org.example.GPT.RedBlackTree;
+
+public class Main {
+    public static void main(String[] args) {
+        org.example.GPT.RedBlackTree node = new RedBlackTree();
+
+        node.insert(1);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(2);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(3);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(4);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(5);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(6);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(7);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(8);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(9);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(10);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(11);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(12);
+        node.inorder();
+    }
+}
